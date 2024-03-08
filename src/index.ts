93553e7ee1d6299b907c629e61283913fcf20bd3
tim1119 <timothyinioluwa@gmail.com>
2024-03-08 04:42:55 +0100

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express'
+import userRoutes from './routes/userRoutes';
+import tweetRoutes from './routes/tweetRoutes';
+
 
 const app = express()
 app.use(express.json())
+app.use('/user',userRoutes)
+app.use('/tweet',tweetRoutes)
 
 app.get('/',(req,res)=>{
-    res.send('Hello World')
+    res.send('Hello World,Updated')
 })
+
 
 app.listen(3000,()=>{
     console.log('Sever ready at localhost:3000')
